028fb59b39abb6a0a9f24e888dfa2d0bf1b1bf41
Nathan Ganser <me@nathanganser.com>
2020-09-17 16:24:57 +0000

    finish simple html version!!

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, send_from_directory
+from flask import Flask, render_template, request, redirect
 import siaskynet as skynet
 import os
 from flask_sqlalchemy import SQLAlchemy
@@ -7,7 +7,7 @@ import webbrowser
 
 
 application = Flask(__name__)
-# application.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE']
+application.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE']
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 db = SQLAlchemy(application)
 client = skynet.SkynetClient()
@@ -18,16 +18,24 @@ class Upload(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     skylink = db.Column(db.String)
     timestamp = db.Column(db.DateTime)
+    title = db.Column(db.String)
+    clicks = db.Column(db.Integer)
+    upvotes = db.Column(db.Integer)
 
     def insert(self):
         db.session.add(self)
         db.session.commit()
 
+    def save(self):
+        db.session.commit()
+
+
 
 # URLs
 @application.route('/')
 def index():
-    return render_template('index.html')
+    uploads = Upload.query.filter(Upload.timestamp > datetime.date.today()).order_by(Upload.clicks.desc()).all()
+    return render_template('index.html', uploads=uploads)
 
 
 @application.route('/upload')
@@ -37,18 +45,24 @@ def upload():
 
 @application.route('/skydirect/<string:skylink>')
 def skydirect(skylink):
-    return webbrowser.open_new_tab("https://siasky.net/" + skylink)
+    upload = Upload.query.filter_by(skylink=skylink).first()
+    upload.clicks += 1
+    upload.save()
+
+    return redirect("https://siasky.net/" + skylink)
 
 
 @application.route('/upload-file', methods=['POST'])
 def uploader():
     if request.files:
-        file = request.files["image"]
-        skylink = client.upload_file(file)
+        file = request.files["file"]
+        file.save(os.path.join("./files", file.filename))
+        title = request.values["title"]
+        skylink = client.upload_file("./files/" + file.filename)
+        os.remove("./files/" + file.filename)
+        clean_link = skylink.replace("sia://", "")
 
-        print("Upload successful, skylink: " + skylink)
-
-        uploaded = Upload(skylink, datetime.datetime.now())
+        uploaded = Upload(skylink=clean_link, timestamp=datetime.datetime.now(), title=title, clicks=0, upvotes=0)
         uploaded.insert()
 
         return redirect('/')
